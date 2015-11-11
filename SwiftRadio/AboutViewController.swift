1097cda4e6270f347579c3ca8eaab9dc261627ab
Mustafa Sahin <m_sahin@windowslive.com>
2015-11-11 22:03:44 +0200

    myTV
    myTV - IOS TV Application

---
+++ SwiftRadio/AboutViewController.swift
@@ -0,0 +1,75 @@
+
+import UIKit
+import MessageUI
+
+class AboutViewController: UIViewController {
+    
+    //*****************************************************************
+    // MARK: - ViewDidLoad
+    //*****************************************************************
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+   
+    //*****************************************************************
+    // MARK: - IBActions
+    //*****************************************************************
+    
+    @IBAction func emailButtonDidTouch(sender: UIButton) {
+        
+        // Use your own email address & subject
+        let receipients = ["info@83colors.com"]
+        let subject = "myTV"
+        let messageBody = ""
+        
+        let configuredMailComposeViewController = configureMailComposeViewController(receipients, subject: subject, messageBody: messageBody)
+        
+        if canSendMail() {
+            self.presentViewController(configuredMailComposeViewController, animated: true, completion: nil)
+        } else {
+            showSendMailErrorAlert()
+        }
+    }
+    
+    @IBAction func websiteButtonDidTouch(sender: UIButton) {
+        
+        // Use your own website here
+        if let url = NSURL(string: "http://83colors.com") {
+            UIApplication.sharedApplication().openURL(url)
+        }
+    }
+
+  }
+
+//*****************************************************************
+// MARK: - MFMailComposeViewController Delegate
+//*****************************************************************
+
+extension AboutViewController: MFMailComposeViewControllerDelegate {
+    
+    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        controller.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func canSendMail() -> Bool {
+        return MFMailComposeViewController.canSendMail()
+    }
+    
+    func configureMailComposeViewController(recepients: [String], subject: String, messageBody: String) -> MFMailComposeViewController {
+        
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self
+        
+        mailComposerVC.setToRecipients(recepients)
+        mailComposerVC.setSubject(subject)
+        mailComposerVC.setMessageBody(messageBody, isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertView(title: "E-posta Gönderilemedi", message: "Cihazınız e-posta gönderemedi. E-posta yapılandırmanızı kontrol edin ve tekrar deneyin.", delegate: self, cancelButtonTitle: "TAMAM")
+        sendMailErrorAlert.show()
+    }
+}
---
+++ SwiftRadio/SwiftRadio-Settings.swift
@@ -0,0 +1,29 @@
+
+import Foundation
+
+//**************************************
+// GENERAL SETTINGS
+//**************************************
+
+// Display Comments
+let DEBUG_LOG = true
+
+//**************************************
+// STATION JSON
+//**************************************
+
+// If this is set to "true", it will use the JSON file in the app
+// Set it to "false" to use the JSON file at the stationDataURL
+
+let useLocalStations = false
+let stationDataURL   = "http://83colors.com/stations.json"
+
+//**************************************
+// LASTFM API
+//**************************************
+
+// USE YOUR OWN API KEY HERE!
+// Visit: http://www.last.fm/api
+
+let apiKey    = "9a267c245324cfa4f887366d497d3dd3"
+let apiSecret = "f1191864d7ae71e580b89238129768b8"
